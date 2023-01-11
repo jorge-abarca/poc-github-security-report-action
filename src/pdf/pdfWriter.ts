f5ecfda4d9d3b454244e9e09025c2e8f8c60126f
jorge-abarca <29783307+jorge-abarca@users.noreply.github.com>
2023-01-11 16:43:26 -0600

    Update pdfWriter.ts

--- src/pdf/pdfWriter.ts
+++ src/pdf/pdfWriter.ts
@@ -4,6 +4,9 @@ const puppeteer = require('puppeteer-core');
 
 export function createPDF(html: string, file: string): Promise<string> {
   
+  console.time("save pdf");
+  console.time("download");
+  
   console.log("Creating pdf...", file);
 
   const fetcher = puppeteer.createBrowserFetcher({path: os.tmpdir()});
@@ -11,15 +14,25 @@ export function createPDF(html: string, file: string): Promise<string> {
   return fetcher.download('782078')//TODO need to store and inject this
     .then(revisionInfo => {
       console.log("Downloaded...", fetcher);
+      console.timeEnd("download");
+      console.log("Launching...");
+      console.time("launch");
       return puppeteer.launch({executablePath: revisionInfo.executablePath})
         .then(browser => {
+          console.log("launched!");
+          console.timeEnd("launch");
           console.log("Creating new page...");
+          console.time("newpage");
           return browser.newPage()
             .then(page => {
+              console.timeEnd("newpage");
               console.log("Setting content...");
+              console.time("set content");
               return page.setContent(html)
                 .then(() => {
+                  console.timeEnd("set content");
                   console.log("Saving as pdf...");
+                  console.timeEnd("save pdf");
                   return page.pdf({path: file, format: 'A4'})
                 });
             })
