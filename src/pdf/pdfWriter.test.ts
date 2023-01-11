5419546afc2230a9d44f9c21b7bd3e6723ad946c
jorge-abarca <29783307+jorge-abarca@users.noreply.github.com>
2023-01-11 16:50:36 -0600

    Update pdfWriter.test.ts

--- src/pdf/pdfWriter.test.ts
+++ src/pdf/pdfWriter.test.ts
@@ -4,18 +4,24 @@ import { createPDF } from './pdfWriter';
 import { getTestDirectoryFilePath } from '../testUtils';
 
 describe('pdfWriter', function () {
-
+  console.log("waiting...");
   this.timeout(30 * 1000);
+  console.log("done waiting!");
 
   it('should generate a simple pdf', async () => {
+    console.log("testing generating a simple file");
     const html = '<html><body><h1>Hello World</h1></body>'
       , file = getTestDirectoryFilePath('test.pdf')
     ;
 
+    console.log("Making directory...");
     // Ensure the directory exists
     await mkdirP(getTestDirectoryFilePath());
 
+    console.log("Generating PDF...");
+    console.time("test.generatePdf");
     const generatePdf = await createPDF(html, file)
+    console.timeEnd("test.generatePdf");
     expect(generatePdf).to.equal(file);
     //TODO check size
   });
